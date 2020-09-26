fcad6d74cf2666992d895ba16be3ea59bf3479b3
Karsten Taylor <karstentaylor0@gmail.com>
2020-09-26 14:33:09 -0700

    fix typo /

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
 	REACT_APP_API_BASE: 'https://karsten-petful.herokuapp.com',
 
-	API_ENDPOINT: 'https://karsten-petful.herokuapp.com/',
+	API_ENDPOINT: 'https://karsten-petful.herokuapp.com',
 };
